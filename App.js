1b073fbe31516f4a3c9a7c9403cda0b13846fd6b
Laura Gomes <laurazt6@gmail.com>
2021-11-26 21:18:16 -0300

    BuscadorDeCep

--- App.js
+++ App.js
@@ -1,21 +1,134 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    SafeAreaView,
+    Keyboard,
+} from "react-native";
+import api from "./src/services/api";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [cep, setCep] = useState("");
+    const inputRef = useRef(null);
+    const [cepUser, setCepUser] = useState(null);
+
+    async function buscar() {
+        if (cep == "") {
+            alert("Digite um cep valido");
+            setCep("");
+            return; //
+        }
+
+        try {
+            const response = await api.get(`/${cep}/json`);
+            console.log(response.data);
+            setCepUser(response.data);
+            Keyboard.dismiss(); //Garantir que o teclado sera fechado!
+        } catch (error) {
+            console.log("ERROR: " + error);
+        }
+    }
+
+    function limpar() {
+        setCep("");
+        inputRef.current.focus();
+        setCepUser(null);
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={{ alignItems: "center" }}>
+                <Text style={styles.text}>Digite o cep desejado</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Ex: 79003241"
+                    value={cep}
+                    onChangeText={(texto) => setCep(texto)}
+                    keyboardType="numeric"
+                    ref={inputRef}
+                />
+            </View>
+
+            <View style={styles.areaBtn}>
+                <TouchableOpacity
+                    style={[styles.botao, { backgroundColor: "#1d75cd" }]}
+                    onPress={buscar}
+                >
+                    <Text style={styles.botaoText}>Buscar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={[styles.botao, { backgroundColor: "#cd3e1d" }]}
+                    onPress={limpar}
+                >
+                    <Text style={styles.botaoText}>Limpar</Text>
+                </TouchableOpacity>
+            </View>
+
+            {cepUser && (
+                <View style={styles.resultado}>
+                    <Text style={styles.itemText}>CEP: {cepUser.cep}</Text>
+                    <Text style={styles.itemText}>
+                        Logradouro: {cepUser.logradouro}
+                    </Text>
+                    <Text style={styles.itemText}>
+                        Bairro: {cepUser.bairro}
+                    </Text>
+                    <Text style={styles.itemText}>
+                        Cidade: {cepUser.localidade}
+                    </Text>
+                    <Text style={styles.itemText}>Estado: {cepUser.uf}</Text>
+                </View>
+            )}
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+    },
+    text: {
+        marginTop: 25,
+        marginBottom: 15,
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+    input: {
+        backgroundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: "#DDD",
+        borderRadius: 5,
+        width: "90%",
+        padding: 10,
+        fontSize: 18,
+    },
+    areaBtn: {
+        alignItems: "center",
+        flexDirection: "row",
+        marginTop: 15,
+        justifyContent: "space-around",
+    },
+    botao: {
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 15,
+        borderRadius: 5,
+    },
+    botaoText: {
+        fontSize: 22,
+        color: "#FFF",
+    },
+    resultado: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    itemText: {
+        fontSize: 22,
+    },
 });
